f902a935c07c1f6b3d72e727f98a6ba39dd3053b
Ocampo323232 <a.socampo32@gmail.com>
2025-09-06 22:10:15 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./app/*.{ts,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
-  plugins: []
-}
+  plugins: [],
+};
